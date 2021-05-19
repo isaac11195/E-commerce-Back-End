4612282a3e7e9f3fa3a752ffc1441863abd2b68b
Mona Lisa <isaac11195@gmail.com>
2021-05-19 10:34:25 -0400

    add new commands

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// sync Sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
