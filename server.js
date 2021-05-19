f4d2fc5a73146f4a0eceb146d5b996b20b88462c
Mona Lisa <isaac11195@gmail.com>
2021-05-19 10:17:08 -0400

    add new folders and files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
